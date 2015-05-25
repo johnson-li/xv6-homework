d353c4e997e74c30df4b1d2d2491214950210ad9
johnson-li <johnsonli1993@163.com>
2015-05-25 22:37:54 +0800

    Homework: xv6 lazy page allocation -- Part One: Eliminate allocation from sbrk()

--- sysproc.c
+++ sysproc.c
@@ -51,8 +51,10 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = proc->sz;
-  if(growproc(n) < 0)
+  /*if(growproc(n) < 0)
     return -1;
+  return addr;*/
+  proc->sz += n;
   return addr;
 }
 
